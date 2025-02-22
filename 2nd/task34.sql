df39cc4ef0c55a3f33232b89be68c2ebc1ee08eb
LuckHost <sh76484@gmail.com>
2025-02-22 09:25:27 +0700

    task34 fix

--- 2nd/task34.sql
+++ 2nd/task34.sql
@@ -6,6 +6,14 @@ CREATE TABLE pilot_hobbies (
     hobbies jsonb
 );
 
+-- Вставка данных в таблицу pilot_hobbies
+INSERT INTO pilot_hobbies (pilot_name, hobbies)
+VALUES 
+    ('Ivan', '{"trips": 3, "home_lib": "Main Library"}'),
+    ('Petr', '{"trips": 2, "home_lib": "City Library"}'),
+    ('Boris', '{"trips": 0, "home_lib": "Town Library"}'),
+    ('Pavel', '{"trips": 5, "home_lib": "Central Library"}');
+
 -- Обновление значения по ключу home_lib для конкретного пилота
 UPDATE pilot_hobbies
 SET hobbies = jsonb_set(
